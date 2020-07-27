a440fdb2583dbc1f055e801dc9ed6978bee9a988
DevJun <guswns1659@gmail.com>
2020-07-27 21:15:59 +0900

    feat  : BinarySearchTree.delete() 구현

--- src/main/java/com/titanic/javatest/binarysearchtree/BinarySearchTree.java
+++ src/main/java/com/titanic/javatest/binarysearchtree/BinarySearchTree.java
@@ -46,4 +46,89 @@ public class BinarySearchTree {
         }
         return false;
     }
+
+    public boolean delete(TreeNode deletingNode) {
+
+        // 삭제하려는 노드가 없는 경우
+        boolean searched = false;
+        TreeNode current = this.head;
+        TreeNode parent = this.head;
+
+        while (current != null) {
+            if (deletingNode.getValue().equals(current.getValue())) {
+                searched = true;
+                break;
+            } else if (deletingNode.getValue() < current.getValue()) {
+                parent = current;
+                current = current.getLeft();
+            } else {
+                parent = current;
+                current = current.getRight();
+            }
+        }
+
+        if (!searched) {
+            return false;
+        }
+
+        // 1. 삭제하는 노드의 자식이 없는 경우
+        if (current.getLeft() == null && current.getRight() == null) {
+            if (deletingNode.getValue() < parent.getValue()) {
+                parent.setLeft(null);
+            } else {
+                parent.setRight(null);
+            }
+            return true;
+        }
+        // 2. 삭제하는 노드의 자식이 1개인 경우
+        // 2-1 삭제하는 노드의 자식이 왼쪽 노드인 경우
+        if (current.getLeft() != null && current.getRight() == null) {
+            if (deletingNode.getValue() < parent.getValue()) {
+                parent.setLeft(current.getLeft());
+            } else {
+                parent.setRight(current.getLeft());
+            }
+            return true;
+        }
+        // 2-2 삭제하는 노드의 자식이 오른쪽 노드인 경우
+        if (current.getLeft() == null && current.getRight() != null) {
+            if (deletingNode.getValue() < parent.getValue()) {
+                parent.setLeft(current.getRight());
+            } else {
+                parent.setRight(current.getRight());
+            }
+            return true;
+        }
+
+        // 3. 삭제하는 노드의 자식이 2개인 경우
+        if (current.getLeft() != null && current.getRight() != null) {
+            TreeNode changeNode = current.getRight();
+            TreeNode changeParentNode = current.getRight();
+            while (changeNode.getLeft() != null) {
+                changeParentNode = changeNode;
+                changeNode = changeNode.getLeft();
+            }
+            // 3-2-1 바뀌는 노드의 자식노드가 없는 경우
+            if (changeNode.getRight() == null) {
+                changeParentNode.setLeft(null);
+            // 3-2-2 바뀌는 노드가 오른쪽 자식 노드가 있는 경우
+            } else {
+                changeParentNode.setLeft(changeNode.getRight());
+            }
+            // 3-1 삭제하는 노드가 부모 노드의 왼쪽에 있는 경우
+            if (current.getValue() < parent.getValue()) {
+                parent.setLeft(changeNode);
+            // 3-2 삭제하는 노드가 부모 노드의 오른쪽에 있는 경우
+            } else {
+                parent.setRight(changeNode);
+            }
+            changeNode.setLeft(current.getLeft());
+            // 3-3 바뀌는 노드와 삭제하려는 노드의 위치를 바꾸는데 바꾼 뒤에 자기 자신이 오른쪽에 안달려있기 위한 조건
+            if (parent.getRight() != changeNode) {
+                changeNode.setRight(current.getRight());
+            }
+            return true;
+        }
+        return false;
+    }
 }
--- src/test/java/com/titanic/javatest/tree/BinarySearchTreeTest.java
+++ src/test/java/com/titanic/javatest/tree/BinarySearchTreeTest.java
@@ -15,6 +15,22 @@ public class BinarySearchTreeTest {
     void 이진탐색트리를_생성한다() {
         TreeNode head = new TreeNode(5);
         this.binarySearchTree = new BinarySearchTree(head);
+
+        for (int i = 1; i < 10; i+=2) {
+            if (i == 5) {
+                continue;
+            }
+            TreeNode treeNode = new TreeNode(i);
+            this.binarySearchTree.add(treeNode);
+        }
+
+        for (int i = 2; i < 7; i+=2) {
+            if (i == 5) {
+                continue;
+            }
+            TreeNode treeNode = new TreeNode(i);
+            this.binarySearchTree.add(treeNode);
+        }
     }
 
     @Test
@@ -27,5 +43,11 @@ public class BinarySearchTreeTest {
 
         assertThat(this.binarySearchTree.find(new TreeNode(9))).isTrue();
         assertThat(this.binarySearchTree.find(new TreeNode(11))).isFalse();
+    }
+
+    @Test
+    void 이진탐색트리의_delete를_테스트한다() {
+        assertThat(this.binarySearchTree.delete(new TreeNode(3))).isTrue();
+        assertThat(this.binarySearchTree.delete(new TreeNode(7))).isTrue();
     }
 }
