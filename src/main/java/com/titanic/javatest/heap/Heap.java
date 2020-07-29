62f3a9167f99299c039c5d1c2513f0ca085f99b8
DevJun <guswns1659@gmail.com>
2020-07-29 14:55:16 +0900

    feat : Heap.pop() 구현

--- src/main/java/com/titanic/javatest/heap/Heap.java
+++ src/main/java/com/titanic/javatest/heap/Heap.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class Heap {
 
     private List<Integer> heap = new ArrayList<>();
+    private final static int ONE = 1;
+    private final static int TWO = 2;
 
     public Heap() {
         this.heap.add(null);
@@ -13,22 +15,64 @@ public class Heap {
 
     public boolean add(int added) {
         this.heap.add(added);
-        int addedIndex = this.heap.size() - 1;
+        int addedIndex = this.heap.size() - ONE;
 
         while (isMoveUp(addedIndex)) {
-            int parentIndex = addedIndex / 2;
+            int parentIndex = addedIndex / TWO;
             swap(parentIndex, addedIndex);
             addedIndex = parentIndex;
         }
         return true;
     }
 
+    public void traverse() {
+        this.heap.forEach(System.out::println);
+    }
+
+    public Integer pop() {
+        if (this.heap.size() <= ONE) {
+            return null;
+        }
+        int returnedData = this.heap.get(ONE);
+        this.heap.remove(ONE);
+        this.heap.add(ONE, this.heap.get(this.heap.size() - ONE));
+        this.heap.remove(this.heap.size() - ONE);
+
+        int poppedIndex = ONE;
+
+        while (isMoveDown(poppedIndex)) {
+            int leftChildIndex = poppedIndex * TWO;
+            int rightChildIndex = poppedIndex * TWO + ONE;
+
+            // 2. 오른쪽 자식 노드만 없을 때
+            if (rightChildIndex >= this.heap.size()) {
+                if (this.heap.get(poppedIndex) < this.heap.get(leftChildIndex)) {
+                    swap(poppedIndex, leftChildIndex);
+                    poppedIndex = leftChildIndex;
+                }
+            }
+            // 3. 왼쪽, 오른쪽 자식 노드 모두 있을 때
+            if (this.heap.get(leftChildIndex) >= this.heap.get(rightChildIndex)) {
+                if (this.heap.get(poppedIndex) < this.heap.get(leftChildIndex)) {
+                    swap(poppedIndex, leftChildIndex);
+                    poppedIndex = leftChildIndex;
+                }
+            } else {
+                if (this.heap.get(poppedIndex) < this.heap.get(rightChildIndex)) {
+                    swap(poppedIndex, rightChildIndex);
+                    poppedIndex = rightChildIndex;
+                }
+            }
+        }
+        return returnedData;
+    }
+
     private boolean isMoveUp(int addedIndex) {
-        if (addedIndex == 1) {
+        if (addedIndex == ONE) {
             return false;
         }
 
-        int parentIndex = addedIndex / 2;
+        int parentIndex = addedIndex / TWO;
         return this.heap.get(addedIndex) > this.heap.get(parentIndex);
     }
 
@@ -41,7 +85,24 @@ public class Heap {
         this.heap.add(addedIndex, parentValue);
     }
 
-    public void traverse() {
-        this.heap.forEach(System.out::println);
+    private boolean isMoveDown(int poppedIndex) {
+        int leftChildIndex = poppedIndex * TWO;
+        int rightChildIndex = poppedIndex * TWO + ONE;
+
+        // 1. 왼쪽 자식 노드도 없을 때 (완전이진트리라서 오른쪽 자식 노드만 있을 수가 없다.)
+        if (leftChildIndex >= this.heap.size()) {
+            return false;
+        }
+        // 2. 오른쪽 자식 노드만 없을 때
+        if (rightChildIndex >= this.heap.size()) {
+            return this.heap.get(poppedIndex) < this.heap.get(leftChildIndex);
+        }
+        // 3. 왼쪽, 오른쪽 자식 노드 모두 있을 때
+        int maxData = Math.max(this.heap.get(leftChildIndex), this.heap.get(rightChildIndex));
+        return this.heap.get(poppedIndex) < maxData;
+    }
+
+    public List<Integer> getHeap() {
+        return heap;
     }
 }
--- src/test/java/com/titanic/javatest/heap/HeapTest.java
+++ src/test/java/com/titanic/javatest/heap/HeapTest.java
@@ -2,6 +2,8 @@ package com.titanic.javatest.heap;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,15 +14,27 @@ public class HeapTest {
     @BeforeEach
     void 힙을_생성한다() {
         this.heap = new Heap();
-    }
-
-    @Test
-    void 힙의_add를_테스트한다() {
 
         for (int odd = 1; odd < 10; odd+=2) {
             assertThat(this.heap.add(odd)).isTrue();
         }
+    }
 
+    @CsvSource({"10, 1"})
+    @ParameterizedTest
+    void 힙의_add를_테스트한다(Integer ten, Integer one) {
+
+        this.heap.add(ten);
+        assertThat(this.heap.getHeap().get(one)).isEqualTo(ten);
+        this.heap.traverse();
+    }
+
+    @Test
+    void 힙의_pop을_테스트한다() {
+
+        this.heap.traverse();
+        assertThat(this.heap.pop()).isEqualTo(9);
+        assertThat(this.heap.pop()).isEqualTo(7);
         this.heap.traverse();
     }
 }
