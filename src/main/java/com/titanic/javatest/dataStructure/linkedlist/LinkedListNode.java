ca1a04629b49e5a4a1ad2182dcd78813f570d3b0
DevJun <guswns1659@gmail.com>
2020-08-04 15:29:53 +0900

    feat : Node를 LinkedListNode로 변경.
    - Node 클래스가 HashTable에서도 사용되서 data, value를 가지고 있는데 LinkedListNode로 변경.

---
+++ src/main/java/com/titanic/javatest/dataStructure/linkedlist/LinkedListNode.java
@@ -0,0 +1,40 @@
+package com.titanic.javatest.dataStructure.linkedlist;
+
+public class LinkedListNode {
+
+    private LinkedListNode prev;
+    private Integer value;
+    private LinkedListNode next;
+
+    public LinkedListNode(Integer value) {
+        this.value = value;
+    }
+
+    public LinkedListNode getPrev() {
+        return prev;
+    }
+
+    public void setPrev(LinkedListNode prev) {
+        this.prev = prev;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public LinkedListNode getNext() {
+        return next;
+    }
+
+    public void setNext(LinkedListNode next) {
+        this.next = next;
+    }
+
+    public boolean isTail() {
+        return this.value != null;
+    }
+}
