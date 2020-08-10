ec0c64385061ab82bca711a808d3dd1b4d67fa60
DevJun <guswns1659@gmail.com>
2020-08-10 12:40:09 +0900

    feat : SelectionSort 리팩토링
    - 알고리즘 반복주기 달성위해 복습.
    - 생각해보면 코드보고 다시 따라치는 연습인데 무엇을 두려워했는지..

--- src/main/java/com/titanic/javatest/algorithm/SelectionSort.java
+++ src/main/java/com/titanic/javatest/algorithm/SelectionSort.java
@@ -2,27 +2,21 @@ package com.titanic.javatest.algorithm;
 
 import java.util.List;
 
+import static java.util.Collections.swap;
+
 public class SelectionSort {
 
     public void sort(List<Integer> unsortedList) {
 
-        for (int stand = 0; stand < unsortedList.size() - 1; stand++) {
+        int unsortedListSize = unsortedList.size();
+        for (int stand = 0; stand < unsortedListSize - 1; stand++) {
             int lowest = stand;
-            for (int compare = stand + 1; compare < unsortedList.size(); compare++) {
+            for (int compare = stand + 1; compare < unsortedListSize; compare++) {
                 if (unsortedList.get(lowest) > unsortedList.get(compare)) {
                     lowest = compare;
                 }
             }
             swap(unsortedList, stand, lowest);
         }
-    }
-
-    private void swap(List<Integer> unsortedList, int stand, int lowest) {
-        int standValue = unsortedList.get(stand);
-        int lowestValue = unsortedList.get(lowest);
-        unsortedList.remove(stand);
-        unsortedList.add(stand, lowestValue);
-        unsortedList.remove(lowest);
-        unsortedList.add(lowest, standValue);
     }
 }
--- src/test/java/com/titanic/javatest/algorithm/SelectionSortTest.java
+++ src/test/java/com/titanic/javatest/algorithm/SelectionSortTest.java
@@ -1,5 +1,6 @@
 package com.titanic.javatest.algorithm;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +10,8 @@ public class SelectionSortTest {
 
     private SelectionSort selectionSort;
     private List<Integer> unsortedList;
+    private long startTime;
+    private long endTime;
 
     @BeforeEach
     void 선택정렬을_생성한다() {
@@ -19,13 +22,20 @@ public class SelectionSortTest {
             randomSet.add(new Random().nextInt(100));
         }
         this.unsortedList = new ArrayList<>(randomSet);
+        this.startTime = System.nanoTime();
     }
 
     @Test
     void 선택정렬을_테스트한다() {
-
         System.out.println(this.unsortedList);
         this.selectionSort.sort(this.unsortedList);
         System.out.println(this.unsortedList);
+    }
+
+    @AfterEach
+    void tearDown() {
+        this.endTime = System.nanoTime();
+        double elapsedTime = (endTime - startTime) / 1000000.0;
+        System.out.println("elapsedTime : " + elapsedTime);
     }
 }
