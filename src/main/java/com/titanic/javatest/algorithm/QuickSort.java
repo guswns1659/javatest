11a9d9c5f74e80c83f473de042aaf41cfa6494d6
DevJun <guswns1659@gmail.com>
2020-08-18 14:49:57 +0900

    feat : QuickSort 알고리즘 리팩토링
    - 반복주기 1회 달성

--- src/main/java/com/titanic/javatest/algorithm/QuickSort.java
+++ src/main/java/com/titanic/javatest/algorithm/QuickSort.java
@@ -5,25 +5,29 @@ import java.util.List;
 
 public class QuickSort {
     public static List<Integer> quickSort(List<Integer> unsorted) {
+
         if (unsorted.size() <= 1) {
             return unsorted;
         }
-        int pivot = unsorted.get(0);
 
+        int pivot = unsorted.get(0);
         List<Integer> left = new ArrayList<>();
         List<Integer> right = new ArrayList<>();
 
         for (int index = 1; index < unsorted.size(); index++) {
-            if (pivot > unsorted.get(index)) {
-                left.add(unsorted.get(index));
+            int data = unsorted.get(index);
+            if (pivot > data) {
+                left.add(data);
             } else {
-                right.add(unsorted.get(index));
+                right.add(data);
             }
         }
+
         left = quickSort(left);
         left.add(pivot);
         right = quickSort(right);
         left.addAll(right);
+
         return left;
     }
 }
