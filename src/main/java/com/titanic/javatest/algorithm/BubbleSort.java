a2e3131ce7696ee1169f303371dfa31ec1f51cf5
DevJun <guswns1659@gmail.com>
2020-08-18 14:58:51 +0900

    feat : Bubblesort 알고리즘 반복주기2회

--- src/main/java/com/titanic/javatest/algorithm/BubbleSort.java
+++ src/main/java/com/titanic/javatest/algorithm/BubbleSort.java
@@ -1,18 +1,21 @@
 package com.titanic.javatest.algorithm;
 
+import java.util.Collections;
 import java.util.List;
 
 import static java.util.Collections.swap;
 
 public class BubbleSort {
+
     public void sort(List<Integer> unsortedList) {
+
         int unsortedListSize = unsortedList.size();
 
         for (int turn = 0; turn < unsortedListSize - 1; turn++) {
             boolean swap = false;
             for (int index = 0; index < unsortedListSize - turn - 1; index++) {
-                if (unsortedList.get(index) > unsortedList.get(index + 1)) {
-                    swap(unsortedList, index, index+1);
+                if (unsortedList.get(index) > unsortedList.get(index+1)) {
+                    Collections.swap(unsortedList, index, index+1);
                     swap = true;
                 }
             }
@@ -20,5 +23,6 @@ public class BubbleSort {
                 break;
             }
         }
+
     }
 }
