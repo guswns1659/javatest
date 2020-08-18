7522b814db1bd987e8e473615f9e620a19fdb722
DevJun <guswns1659@gmail.com>
2020-08-18 15:11:32 +0900

    feat : SelectionSort 알고리즘 반복주기 2회

--- src/main/java/com/titanic/javatest/algorithm/SelectionSort.java
+++ src/main/java/com/titanic/javatest/algorithm/SelectionSort.java
@@ -1,14 +1,14 @@
 package com.titanic.javatest.algorithm;
 
+import java.util.Collections;
 import java.util.List;
-
-import static java.util.Collections.swap;
 
 public class SelectionSort {
 
-    public void sort(List<Integer> unsortedList) {
 
+    public void sort(List<Integer> unsortedList) {
         int unsortedListSize = unsortedList.size();
+
         for (int stand = 0; stand < unsortedListSize - 1; stand++) {
             int lowest = stand;
             for (int compare = stand + 1; compare < unsortedListSize; compare++) {
@@ -16,7 +16,7 @@ public class SelectionSort {
                     lowest = compare;
                 }
             }
-            swap(unsortedList, stand, lowest);
+            Collections.swap(unsortedList, stand, lowest);
         }
     }
 }
