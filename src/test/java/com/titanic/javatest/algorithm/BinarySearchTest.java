85e96debb9ba46a0bb3b55835cf04d24c40ac392
DevJun <guswns1659@gmail.com>
2020-08-24 20:54:14 +0900

    feat : TimeUnit 이용해서 나노타임을 밀리세컨드로 변경.

--- src/test/java/com/titanic/javatest/algorithm/BinarySearchTest.java
+++ src/test/java/com/titanic/javatest/algorithm/BinarySearchTest.java
@@ -1,5 +1,6 @@
 package com.titanic.javatest.algorithm;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -8,6 +9,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,21 +17,31 @@ public class BinarySearchTest {
 
     private List<Integer> list;
     private BinarySearch binarySearch;
+    private long startTime;
+    private long endTime;
 
     @BeforeEach
     void setUp() {
-        this.list = new ArrayList<>();
-        this.binarySearch = new BinarySearch();
+        list = new ArrayList<>();
+        binarySearch = new BinarySearch();
 
         for (int number = 0; number < 101; number++) {
-            this.list.add(number);
+            list.add(number);
         }
+        startTime = System.nanoTime();
     }
 
     @ParameterizedTest
-    @CsvSource({"78", "200"})
+    @CsvSource({"78"})
     void 이진탐색을_테스트한다(int goal) {
-        assertThat(this.binarySearch.search(this.list, goal)).isTrue();
+        assertThat(binarySearch.search(list, goal)).isTrue();
+    }
+
+    @AfterEach
+    void tearDown() {
+        endTime = System.nanoTime();
+        long convert = TimeUnit.MILLISECONDS.convert((endTime - startTime), TimeUnit.NANOSECONDS);
+        System.out.println("time : " + convert);
     }
 
 }
