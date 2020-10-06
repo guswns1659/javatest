d9ab641e78e7729cf624d884b455ef39cef0d0f6
DevJun <guswns1659@gmail.com>
2020-10-06 14:38:36 +0900

    feat : 바킹독 트리 dfs 구현

--- src/main/java/com/titanic/javatest/barkingdog/tree/Tree.java
+++ src/main/java/com/titanic/javatest/barkingdog/tree/Tree.java
@@ -17,12 +17,30 @@ public class Tree {
         tree.put(4, Collections.singletonList(2));
         tree.put(5, Collections.singletonList(2));
 
-        parentBfs(1);
-        depthBfs(1);
+        bfs(1);
+        parent = new int[6];
+        depth = new int[6];
+        dfs(1);
         System.out.println(Arrays.toString(depth));
     }
 
-    private static void depthBfs(int root) {
+    private static void dfs(int root) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            int current = stack.pop();
+
+            for (int next : tree.get(current)) {
+                if (parent[current] == next) continue;
+                parent[next] = current;
+                stack.push(next);
+                depth[next] = depth[current] + 1;
+            }
+        }
+    }
+
+    private static void bfs(int root) {
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(root);
 
@@ -38,20 +56,4 @@ public class Tree {
         }
     }
 
-    public static void parentBfs(int root) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(root);
-
-        while (!queue.isEmpty()) {
-            int current = queue.poll();
-
-            // 연결된 노드를 순회하고 자식 노드면 queue에 넣는다.
-            for (int next : tree.get(current)) {
-                // 부모 노드일 경우
-                if (parent[current] == next) continue;
-                queue.offer(next);
-                parent[next] = current;
-            }
-        }
-    }
 }
