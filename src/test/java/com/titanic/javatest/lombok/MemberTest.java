830fbf6b895cf1854f761e1e8a464494b2497385
DevJun <guswns1659@gmail.com>
2020-10-19 12:08:57 +0900

    feat : 롬복 동작원리 실습

---
+++ src/test/java/com/titanic/javatest/lombok/MemberTest.java
@@ -0,0 +1,13 @@
+package com.titanic.javatest.lombok;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MemberTest {
+
+    @Test
+    public void getterSetter() {
+        Member member = new Member();
+    }
+}
