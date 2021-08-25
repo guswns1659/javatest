f0267bb2474a529a836b82e45d67606649b71736
guswns1659 <guswns1659@gmail.com>
2021-08-25 22:38:48 +0900

    Test javap command

--- src/main/java/com/titanic/javatest/lambda/LambdaTest.java
+++ src/main/java/com/titanic/javatest/lambda/LambdaTest.java
@@ -6,8 +6,8 @@ import java.util.List;
 public class LambdaTest {
 
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1 ,2, 3);
-        list.stream().filter(integer -> integer > 2).forEach(System.out::println);
-    }
+        long lengthyColors = Arrays.asList("Red", "Green", "Blue")
+            .stream().filter(c -> c.length() > 3).count();
 
+    }
 }
