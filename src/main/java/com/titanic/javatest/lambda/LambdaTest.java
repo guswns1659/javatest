6916f5001de86c060e62598fda0d2a56779c0e42
guswns1659 <guswns1659@gmail.com>
2021-08-29 22:57:29 +0900

    Add scanner for testing visualVM

--- src/main/java/com/titanic/javatest/lambda/LambdaTest.java
+++ src/main/java/com/titanic/javatest/lambda/LambdaTest.java
@@ -1,21 +1,24 @@
 package com.titanic.javatest.lambda;
 
+import java.io.BufferedInputStream;
+import java.lang.invoke.CallSite;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Scanner;
 
 public class LambdaTest {
 
     public static void main(String[] args) {
         LambdaObject lambdaObject = new LambdaObject();
-        System.out.println(lambdaObject.lengthyColors("Red", "Green", "Blue"));
+        System.out.println(lambdaObject.checkLength("Red", "Green", "Blue"));
+        Scanner scanner = new Scanner(System.in);
+        String next = scanner.next();
     }
 
     static class LambdaObject {
 
-        public long lengthyColors(String color1, String color2, String color3) {
-            long lengthyColors = Arrays.asList(color1, color2, color3)
+        public long checkLength(String color1, String color2, String color3) {
+            return Arrays.asList(color1, color2, color3)
                 .stream().filter(c -> c.length() > 3).count();
-            return lengthyColors;
         }
     }
 }
