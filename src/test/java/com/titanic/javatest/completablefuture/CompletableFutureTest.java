1ade29dd3fee2edbb2fe7e560520ebe542dfc054
guswns1659 <guswns1659@gmail.com>
2022-07-06 07:48:56 +0900

    feat: completableFuture fail test draft

--- src/test/java/com/titanic/javatest/completablefuture/CompletableFutureTest.java
+++ src/test/java/com/titanic/javatest/completablefuture/CompletableFutureTest.java
@@ -5,28 +5,53 @@ import org.junit.jupiter.api.Test;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeoutException;
 
 class CompletableFutureTest {
     private static final int sleepA = 3;
     private static final int sleepB = 1;
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(4);
+    private static final long payAccountId = 1L;
 
     @Test
-    void single() {
-        ExecutorService executorService = Executors.newFixedThreadPool(4);
-
-        long payAccountId = 1L;
+    void success() {
         long start = System.currentTimeMillis();
 
         CompletableFuture.allOf(
                 CompletableFuture.supplyAsync(
                         () -> select(payAccountId), executorService
-                ).thenAcceptAsync(this::insert),
+                ).thenAccept(this::insert),
 
                 CompletableFuture.supplyAsync(
                         () -> select2(payAccountId), executorService
-                ).thenAcceptAsync(this::insert2)
+                ).thenAccept(this::insert2)
         ).join();
+
+        // send completed kafka
         System.out.println("All elasped : " + ((System.currentTimeMillis() - start) / 1000) + "s");
+    }
+
+    @Test
+    void fail() {
+        CompletableFuture.allOf(
+                CompletableFuture.supplyAsync(
+                        () -> selectWithTimeout(payAccountId)
+                ).thenAccept(this::insert),
+
+                CompletableFuture.supplyAsync(
+                        () -> select2(payAccountId)
+                ).thenAccept(this::insert2)
+        ).join();
+    }
+
+    private long selectWithTimeout(long payAccountId) {
+
+        try {
+            Thread.sleep(sleepA * 1000);
+            throw new TimeoutException("timeout");
+        } catch (Exception e) {
+        }
+        return payAccountId;
     }
 
     private Long select(long payAccountId) {
